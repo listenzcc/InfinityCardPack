7d866ceaac1f757a3bef7a728064052868261702
listenzcc <cc.public@outlook.com>
2024-08-13 16:55:16 +0800

    Move to docs

--- watch-tailwind.ps1
+++ watch-tailwind.ps1
@@ -2,4 +2,4 @@
 # https://tailwindcss.com/docs/installation
 
 # Watch for changes
-npx tailwindcss -i ./src/input.css -o ./src/css/output.css --watc
+npx tailwindcss -i ./docs/input.css -o ./docs/css/output.css --watch
